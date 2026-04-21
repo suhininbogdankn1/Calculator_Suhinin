024c5e1fe0421836c26609b575dd381fc909d95c
suhininbogdankn1 <suhininbogdan13@gmail.com>
2026-04-21 18:58:39 +0300

    Калькулятор

--- Calculator.py
+++ Calculator.py
@@ -3,12 +3,11 @@ import customtkinter as ctk
 root = ctk.CTk()
 root.title("Calculator")
 root.geometry("500x300")
-root.grid_columnconfigure((0, 1, 2, 3), weight=1)
-
-entry1 = ctk.CTkEntry(root, width=100)
+root.grid_columnconfigure((0,1,2,3), weight=1)
+entry1 = ctk.CTkEntry(root, width=100,height=60,font=("Arial",24))
 entry1.grid(row=0, column=1, padx=5, pady=5)
 
-entry2 = ctk.CTkEntry(root, width=100)
+entry2 = ctk.CTkEntry(root, width=100,height=60,font=("Arial",24))
 entry2.grid(row=0, column=2, padx=5, pady=5)
 
 operation = None
@@ -53,7 +52,7 @@ def calculate():
         else:
             result = "Оберіть операцію (+, -, *, /)"
     except ValueError:
-        result_label.configure(text="Результат: Введіть числа")
+        result_label.configure(text="Введіть числа")
         return
 
     result_label.configure(text=f"Результат: {result}")
@@ -61,21 +60,21 @@ def calculate():
 
 
 equalsbutton = ctk.CTkButton(root, fg_color="blue", text="=", command=calculate, font=("Arial", 16))
-equalsbutton.grid(row=2, column=0, columnspan=4, pady=10)
+equalsbutton.grid(row=3, column=0, columnspan=4, pady=10)
 
-plusbutton = ctk.CTkButton(root, fg_color="green", text="+", command=set_plus, font=("Arial", 16))
+plusbutton = ctk.CTkButton(root, fg_color="green", text="+", command=set_plus, font=("Arial", 16),width=75, height=60,)
 plusbutton.grid(row=1, column=0)
 
-minusbutton = ctk.CTkButton(root, fg_color="green", text="-", command=set_minus, font=("Arial", 16))
+minusbutton = ctk.CTkButton(root, fg_color="green", text="-", command=set_minus, font=("Arial", 16),width=75, height=60,)
 minusbutton.grid(row=1, column=1)
 
-multiplybutton = ctk.CTkButton(root, fg_color="green", text="*", command=set_multiply, font=("Arial", 16))
+multiplybutton = ctk.CTkButton(root, fg_color="green", text="*", command=set_multiply, font=("Arial", 16),width=75, height=60,)
 multiplybutton.grid(row=1, column=2)
 
-dividebutton = ctk.CTkButton(root, fg_color="green", text="/", command=set_division, font=("Arial", 16))
+dividebutton = ctk.CTkButton(root, fg_color="green", text="/", command=set_division, font=("Arial", 16),width=75, height=60,)
 dividebutton.grid(row=1, column=3)
 
 result_label = ctk.CTkLabel(root, text="", font=("Arial", 24))
-result_label.grid(row=3, column=0, columnspan=4, pady=10)
+result_label.grid(row=4, column=0, columnspan=4, pady=10)
 
 root.mainloop()
